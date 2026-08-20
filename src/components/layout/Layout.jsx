9908344abdbb405848bb90d18ae1241e16e4abbb
businessgpt02-code <businessgpt02@gmail.com>
2026-08-20 10:50:50 +0400

    s

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ import FloatingWhatsApp from '../ui/FloatingWhatsApp';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-mme-deepBlack text-mme-white">
+    <div className="flex flex-col min-h-screen bg-mme-deepBlack text-mme-white overflow-x-hidden">
       <Navbar />
       <main className="flex-grow">
         {children}
